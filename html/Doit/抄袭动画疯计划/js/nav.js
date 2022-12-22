920e870c62cafb48da56523c8fdf2aa160025213
OnonokiYotsuki <104435955+OnonokiYotsuki@users.noreply.github.com>
2022-12-22 16:27:47 +0800

    ggo

--- html/Doit/抄袭动画疯计划/js/nav.js
+++ html/Doit/抄袭动画疯计划/js/nav.js
@@ -1,21 +1,22 @@
 $(document).ready(function()  {
     let f=1;
-    setInterval(function() {
-        if ($(window).width() < 810) {
-            $('.bottom').on('click',isBottom)
-            function isBottom() {
-               if(f==1){$('.main-menu').css('display', 'flex');
-               f=0}
-              else{
-                $('.main-menu').css('display', 'none');
-                f=1
-              }
-            };
-        }else{
-            $('.bottom').off('click',isBottom)
-            isMainMenu =$('.main-menu').css('display')
-            if(isMainMenu=='none'){$('.main-menu').css('display', 'flex')}
+    $(window).resize(function() {
+      if ($(window).width() <=810) {
+         $('.bottom').on('click',isBottom)
+    }else{
+         $('.bottom').off('click',isBottom)
+         $('.main-menu').css('display', 'flex');
+    }
+    });
+      function isBottom() {
+        if(f==1){
+          f=0;
+         $('.main-menu').css('display', 'flex');
         }
-      }, 1);
+       else{
+        f=1;
+         $('.main-menu').css('display', 'none');
+       }
+     };
     
 })
