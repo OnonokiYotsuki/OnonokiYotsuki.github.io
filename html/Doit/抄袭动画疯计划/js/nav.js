b868b8bfff937d58fecd764d4386dfe463cbf8ad
OnonokiYotsuki <104435955+OnonokiYotsuki@users.noreply.github.com>
2022-12-22 20:18:20 +0800

    asd

--- html/Doit/抄袭动画疯计划/js/nav.js
+++ html/Doit/抄袭动画疯计划/js/nav.js
@@ -1,35 +1,36 @@
-$(document).ready(function()  {
+$(document).ready(function () {
   /* 响应式nav */
-  let f=0;
-  if ($(window).width()<=810){
-    $('.bottom').on('click',isBottom)
+  let f = 0;
+  if ($(window).width() <= 810) {
+    $('.bottom').on('click', isBottom)
   }
-  $(window).resize(function() {
-    if ($(window).width()<=810) {
-      $('.bottom').on('click',isBottom)
-    }else{
-      $('.bottom').off('click',isBottom)
-      $('.main-menu').css('display', 'flex');
+  $(window).resize(function () {
+    if ($(window).width() <= 810) {
+      $('.bottom').on('click', isBottom)
+    } else {
+      $('.bottom').off('click', isBottom)
     }
   });
   function isBottom() {
-    if(f==1){
-    f=0;
-    $('.main-menu').css('display', 'flex');
+    if (f == 1) {
+      f = 0;
+      $('.main-menu').css('display', 'flex');
     }
-    else{
-    f=1;
-     $('.main-menu').css('display', 'none');
+    else {
+      f = 1;
+      $('.main-menu').css('display', 'none');
     }
   };
   /* 深色模式实现 */
-  $('.dark_swift_button').click(function() {
-    if($('.radio').css('left')=='4px'){
-      $('.radio').css('left','28px')
+  $('.dark_swift_button').click(function (e) {
+    /* 阻止点击触发父级的点击事件 */
+    e.stopPropagation();
+    if ($('.radio').css('left') == '4px') {
+      $('.radio').css('left', '28px')
       $('nav').css('background-color', '#000')
       $('body').css('background-color', '#000')
-    }else{
-      $('.radio').css('left','4px')
+    } else {
+      $('.radio').css('left', '4px')
       $('nav').css('background-color', '#fff')
       $('body').css('background-color', '#fff')
     }
