19337c0e5b93500f0f4670d5a64cb1bea71701f2
OnonokiYotsuki <104435955+OnonokiYotsuki@users.noreply.github.com>
2022-12-23 15:49:50 +0800

    asd

--- html/Doit/抄袭动画疯计划/js/nav.js
+++ html/Doit/抄袭动画疯计划/js/nav.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
   /* 响应式nav */
   let f = 0;
-  if ($(window).width() <= 810) {
+  let flag
+  if($('.menu-icon').css('display') == 'flex'){
     $('.bottom').on('click', isBottom)
   }
   $(window).resize(function () {
-    if ($(window).width() <= 810) {
-      $('.bottom').on('click', isBottom)
-    } else {
+    /* 大屏模式 */
+    if($('.menu-icon').css('display') == 'none') {
+      $('.main-menu').css('display', 'flex');
       $('.bottom').off('click', isBottom)
+      flag=true;
+    } /* 小屏模式 */
+    if($('.menu-icon').css('display') == 'flex'){
+      $('.bottom').on('click', isBottom)
+      hideMenu(flag);
+      flag=false;
     }
   });
   function isBottom() {
@@ -21,6 +28,11 @@ $(document).ready(function () {
       $('.main-menu').css('display', 'none');
     }
   };
+  function hideMenu(a) {
+    if(a){
+      $('.main-menu').css('display', 'none');
+    }
+  }
   /* 深色模式实现 */
   $('.dark_swift_button').click(function (e) {
     /* 阻止点击触发父级的点击事件 */
