9bce2d169d4062427b3c990325f6934ed93a6090
OnonokiYotsuki <104435955+OnonokiYotsuki@users.noreply.github.com>
2022-12-30 21:08:54 +0800

    Update profile.js

--- html/Doit/抄袭动画疯计划/js/profile.js
+++ html/Doit/抄袭动画疯计划/js/profile.js
@@ -1,12 +1,16 @@
 $(function () {
     $('#btnEditPsw').click(function () {
         editKeyValue('userInfo', 'id', read('loggingAccountID'), 'password', $('#inputEditPsw').val())
+        alert('修改成功！')
     })
     $('#btnEditHeadIcon').click(function () {
         editKeyValue('userInfo', 'id', read('loggingAccountID'), 'headIcon', $('#inputEditHeadIcon').val())
+        alert('修改成功！')
     })
     $('#LoginOut').click(function () {
         removeItem('loggingAccountID')
+        alert('已退出账号！')
         window.location.href = 'https://ononokiyotsuki.github.io/html/Doit/抄袭动画疯计划/index.html';
+
     });
 })
